fc7f556c9c3e97963c760c8e24f5fbcbc6dcd525
Ian Rodriguez <ian.rgz@gmail.com>
2012-12-28 18:30:27 -0800

    Finished validation tests and building authorize url

--- lib/harvest/oauth_client.rb
+++ lib/harvest/oauth_client.rb
@@ -1,13 +1,36 @@
+require 'oauth2'
+require 'rack'
+
 module Harvest
   class OAuthClient
-    attr_accessor :oauth_credentials
+    attr_accessor :oauth_credentials, :oauth, :authorize_url
 
     def initialize(oauth_credentials)
       @oauth_credentials = oauth_credentials
+      @oauth = OAuth2::Client.new(@oauth_credentials.client_id, @oauth_credentials.client_secret, site: site, authorize_url: '/oauth2/authorize', token_url: '/oauth2/token')
     end
 
     def valid?
       oauth_credentials.is_a?(Harvest::OAuthCredentials)
+    end
+
+    #Builds the correct authorize url endpoint
+    def authorize_url redirect_uri
+      @authorize_url = oauth.auth_code.authorize_url(redirect_uri: redirect_uri, state: 'optional-csrf-token')
+    end
+
+    #Sets the site to which requests are going
+    def site
+      "https://#{subdomain}.harvestapp.com"
+    end
+
+    #TODO: Test that method redirects to correct url
+    #Not sure if this method is going to work
+    def redirect
+      r = Rack::Response.new
+      r.write("You're being redirected to harvest endpoint...")
+      r.redirect(authorize_url)
+      r.finish
     end
   end
 end
--- spec/harvest/oauth_spec.rb
+++ spec/harvest/oauth_spec.rb
@@ -2,13 +2,16 @@ require 'spec_helper'
 
 #Harvest Oauth Credentials
 describe Harvest::OAuthCredentials do
+  let(:valid_credentials) { Harvest::OAuthCredentials.new('someclientid', 'someclientsecret') }
+  let(:invalid_credentials) { Harvest::OAuthCredentials.new(nil, ENV['HARVEST_SECRET']) }
+
   context '#Validations' do
     it 'should be valid given a client id and client secret' do
-      Harvest::OAuthCredentials.new('someclientid', 'someclientsecret').should be_valid
+      valid_credentials.should be_valid
     end
 
     it 'should not be valid without a client id and client secret' do
-      Harvest::OAuthCredentials.new(nil, ENV['HARVEST_SECRET']).should_not be_valid
+      invalid_credentials.should_not be_valid
       Harvest::OAuthCredentials.new(ENV['HARVEST_IDENTIFIER'], nil).should_not be_valid
     end
   end
@@ -19,6 +22,15 @@ describe Harvest::OAuthClient do
   context '#validations' do
     it 'should be valid given the proper oauth credentials' do
       Harvest::OAuthClient.new(FactoryGirl.build(:oauth_credentials)).should be_valid
+    end
+  end
+
+  context '#requests' do
+    it 'should build the authorize URL given the proper credentials' do
+      client = Harvest::OAuthClient.new(FactoryGirl.build(:oauth_credentials))
+      authorize_url = client.authorize_url('http://supercoolsite.com')
+      authorize_url.length.should >= 0
+      puts authorize_url
     end
   end
 end
